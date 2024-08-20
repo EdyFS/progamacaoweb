03c9641646aa58d0c4b685c2e1bf242711ae27cf
EdyFS <edy.ferreira.santana@gmail.com>
2024-08-19 21:23:53 -0300

    get set

--- POO/Livro.php
+++ POO/Livro.php
@@ -1,5 +1,45 @@
 <?php
 
 class Livro {
-    
+    private $titulo;
+    private $autor;
+    private $ano;
+
+    public function __construct($titulo, $autor, $ano){ //construtor
+        // __ é um chamado método mágico
+        $this->titulo = $titulo; //referenciar, $vai no this e não no atributo
+        $this->autor = $autor;
+        $this->ano = $ano;
+
+    }
+
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function getTitulo(){
+        return $this->titulo;
+    }
+
+    public function setAutor($autor){
+        $this->autor = $autor;
+    }
+
+    public function getAutor(){
+        return $this->autor;
+    }
+
+    public function setAno($ano){
+        if ($ano < 1900)
+            $ano = 1900;
+        $this->ano = $ano;
+    }
+
+    public function getAno(){
+        return $this->ano;
+    }
+
+
 }
+
+$obj = new Livro("O Livro", "Ana", "2024");
