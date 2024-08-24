4d1054b11707f68bc8bb1ab79008261dd964ec57
EdyFS <edy.ferreira.santana@gmail.com>
2024-08-24 09:08:21 -0300

    Exercicios

---
+++ Lista de Exercícios/Exercicio1.php
@@ -0,0 +1,22 @@
+<?php
+
+class Ponto{
+    private X;
+    private Y;
+    public function getX(){
+
+    }
+    public function setX(){
+
+    }
+    public function getY(){
+
+    }
+    public function setY(){
+
+    }
+
+    function deslocar(dx:int, ){
+
+    }
+}
