3c9480ac423426edf6a7cee9738633becd5875ee
EdyFS <edy.ferreira.santana@gmail.com>
2024-08-24 08:27:27 -0300

    Criar classes

---
+++ POO/dados/Usuario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace POO\dados;
+
+class Usuario {
+    private $email = "v@v.com";
+    public function exibirEmail(){
+        echo $this->email;
+    }
+}
---
+++ POO/programa.php
@@ -0,0 +1,10 @@
+<?php
+
+include "Usuario.php";
+include "dados/Usuario.php";
+
+use POO\Usuario as UsuarioRaiz;
+
+use POO\dados\Usuario as UsuarioDados;
+
+$obj = new Usuario();
